849665ce37a153ccc7d53551832b633149776844
Dmitrii Marin <dmitry.marin@gmail.com>
2016-03-15 15:21:25 -0400

    "fixed RSA matrix"

--- dmarin_rsam.m
+++ dmarin_rsam.m
@@ -1,8 +1,26 @@
 function RSA = dmarin_rsam(Y)
 
-G = Y*Y';
-I = reshape(1:9*9, 9, 9);
-Ind = repmat(I, 6) - blkdiag(I,I,I,I,I,I);
-RSA = reshape(accumarray(Ind(Ind>0), G(Ind>0)), 9, 9) / 6;
+%% CV Distances
+Y = reshape(Y, [9, size(Y, 1)/9, size(Y, 2)]);
+sz = size(Y);
+
+Diff = bsxfun(@minus,  ...
+    reshape(Y, [ 1 sz]), ...
+    reshape(Y, [sz(1) 1 sz(2:3)]));
+
+Prod = bsxfun(@times, ...
+    reshape(Diff, [sz(1) sz(1:2) 1 sz(3)]), ...
+    reshape(Diff, [sz(1) sz(1) 1 sz(2:3)]));
+
+D = sum(Prod, 5);
+D = reshape(D, [sz([1 1]), sz(2)^2]);
+D = mean(D(:,:,ones(sz(2)) ~= eye(sz(2))), 3);
+
+%% moment matrix
+
+H = eye(size(D)) - ones(size(D)) / size(D, 1);
+RSA = -H * D * H;
+
+
 
 
