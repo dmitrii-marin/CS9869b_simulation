fa74480cab6d87b65d3211997b1a6e0ea0e00562
harrisonritz <harrison@hritz.local>
2016-03-29 12:43:56 -0400

    %%%
    commented out classification

--- h_test_pattern.m
+++ h_test_pattern.m
@@ -77,84 +77,84 @@ for b = 1:size(beta,2)
     out.h1(2,b) = mean(beta(:,b) > prctile(beta_perm(:,b),95));
 end
 
-%% Run classification simulations
-disp('calculating classification fit')
-tic;
-
-nMain = 3; %levels of each main effect
-pair = nchoosek(1:nMain,2);
-nPair = length(pair);
-
-totPair = nchoosek(1:nCond,2); %total pairwise comparisons
-ntPair = length(totPair);
-
-for i = 1:nRep
-    
-    dat = simulate(pattern(1),pattern(2),pattern(3),'sigmaNoise',1,'Cauchy', 0);
-    dat = permute(reshape(dat, [nMain,nMain,nRun,nVx]), [2, 1, 4, 3]); %make into [main1, main2, nVx, nRun]
-    
-    
-    %% main 1  test
-    
-    for p = 1:nPair; %which levels of main 1 to classify
-        
-        for c = 1:nMain; %which levels of main 2 to collapse over during training
-
-            trainDat = squeeze(mean(dat(pair(p,:), [1:nMain] ~= c, :, :),2));
-            testDat = squeeze(dat(pair(p,:), c, :, :));
-            
-            acc(p,c) = h_classify(trainDat, testDat); %should be able to replace with k-nn 
-            
-        end
-    end
-    
-    m1Class(i) = mean(acc(:));
-    
-    
-    %% main 2 test
-    
-    for p = 1:nPair; %which levels of main 2 to classify
-        
-        for c = 1:nMain; %which levels of main 1 to collapse over during training
-                   
-            trainDat = squeeze(mean(dat([1:nMain] ~= c, pair(p,:), :, :),1));
-            testDat = squeeze(dat(c,pair(p,:), :, :));
-            
-            acc(p,c) = h_classify(trainDat, testDat); %should be able to replace with k-nn 
-            
-        end
-    end
-    
-    m2Class(i) = mean(acc(:));
-    
-    
-    %% Interaction test
-        
-    tempDat = dat;
-    for r = 1:nMain %remove row patterns
-        tempDat(r,:,:,:) = bsxfun(@minus,tempDat(r,:,:,:),mean(dat(r,:,:,:),2));
-    end
-    
-    for a = 1:nMain %remove column patterns
-        tempDat(:,a,:,:) = bsxfun(@minus,tempDat(:,a,:,:),mean(dat(:,a,:,:),1));
-    end    
-    
-    iDat = reshape(tempDat,[nCond,nVx,nRun]);
-    
-    for p = 1:ntPair
-        iAcc(p) = h_classify(iDat(totPair(p,:),:,:),iDat(totPair(p,:),:,:));
-    end
-    
-    iClass(i) = mean(iAcc);
-    
-end
-
-out.m1Class = m1Class;
-out.m2Class = m2Class;
-out.iClass = iClass;
-
-
-toc
+% %% Run classification simulations
+% disp('calculating classification fit')
+% tic;
+% 
+% nMain = 3; %levels of each main effect
+% pair = nchoosek(1:nMain,2);
+% nPair = length(pair);
+% 
+% totPair = nchoosek(1:nCond,2); %total pairwise comparisons
+% ntPair = length(totPair);
+% 
+% for i = 1:nRep
+%     
+%     dat = simulate(pattern(1),pattern(2),pattern(3),'sigmaNoise',1,'Cauchy', 0);
+%     dat = permute(reshape(dat, [nMain,nMain,nRun,nVx]), [2, 1, 4, 3]); %make into [main1, main2, nVx, nRun]
+%     
+%     
+%     %% main 1  test
+%     
+%     for p = 1:nPair; %which levels of main 1 to classify
+%         
+%         for c = 1:nMain; %which levels of main 2 to collapse over during training
+% 
+%             trainDat = squeeze(mean(dat(pair(p,:), [1:nMain] ~= c, :, :),2));
+%             testDat = squeeze(dat(pair(p,:), c, :, :));
+%             
+%             acc(p,c) = h_classify(trainDat, testDat); %should be able to replace with k-nn 
+%             
+%         end
+%     end
+%     
+%     m1Class(i) = mean(acc(:));
+%     
+%     
+%     %% main 2 test
+%     
+%     for p = 1:nPair; %which levels of main 2 to classify
+%         
+%         for c = 1:nMain; %which levels of main 1 to collapse over during training
+%                    
+%             trainDat = squeeze(mean(dat([1:nMain] ~= c, pair(p,:), :, :),1));
+%             testDat = squeeze(dat(c,pair(p,:), :, :));
+%             
+%             acc(p,c) = h_classify(trainDat, testDat); %should be able to replace with k-nn 
+%             
+%         end
+%     end
+%     
+%     m2Class(i) = mean(acc(:));
+%     
+%     
+%     %% Interaction test
+%         
+%     tempDat = dat;
+%     for r = 1:nMain %remove row patterns
+%         tempDat(r,:,:,:) = bsxfun(@minus,tempDat(r,:,:,:),mean(dat(r,:,:,:),2));
+%     end
+%     
+%     for a = 1:nMain %remove column patterns
+%         tempDat(:,a,:,:) = bsxfun(@minus,tempDat(:,a,:,:),mean(dat(:,a,:,:),1));
+%     end    
+%     
+%     iDat = reshape(tempDat,[nCond,nVx,nRun]);
+%     
+%     for p = 1:ntPair
+%         iAcc(p) = h_classify(iDat(totPair(p,:),:,:),iDat(totPair(p,:),:,:));
+%     end
+%     
+%     iClass(i) = mean(iAcc);
+%     
+% end
+% 
+% out.m1Class = m1Class;
+% out.m2Class = m2Class;
+% out.iClass = iClass;
+% 
+% 
+% toc
 
 
 
