b9dd935043a62c9d247b509c0523f06cebff53aa
Dmitrii Marin <dmitry.marin@gmail.com>
2016-03-30 10:33:33 -0400

    added missing file

---
+++ pr.m
@@ -0,0 +1,9 @@
+function pr( output )
+
+set(gcf,'Units','Inches');
+pos = get(gcf,'Position');
+set(gcf,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+print ('-dpdf', output);
+
+end
+
